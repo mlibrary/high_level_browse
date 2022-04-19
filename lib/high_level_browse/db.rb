6af3ed8bfeb2022191b75f7d8900f470c68dbf7c
Bill Dueber <bill@dueber.com>
2022-04-19 15:56:16 -0400

    BREAKING CHANGE
    The on-disk representation of the HLB (the `hlb.json.gz` file) has changed.
    Using this version will require that you re-fetch the hlb
    (with `fetch_new_hlb <dest_dir>`).
    
    BUG FIXES
    * Protects against nil/empty queries. This was causing a range object `nil..nil` to be created,
      which throws an error in modern jruby implementations.
    
    A couple important internal-only changes
    
    * Replace oga with nokogiri. Nokogiri is installed in pretty much everything we do anyway.
    * Change the call-number normalization process to be better. Instead of using `lcsort`,
      now uses code essentially copied from https://github.com/mlibrary/library_identifier_solr_filters/blob/main/src/main/edu/umich/library/library_identifier/normalizers/LCCallNumberSimple.java
    
    This also removes non-working test and benchmark code, both of which should
    be addressed at some point.

--- lib/high_level_browse/db.rb
+++ lib/high_level_browse/db.rb
@@ -1,4 +1,4 @@
-require 'oga'
+require 'nokogiri'
 require 'high_level_browse/call_number_range'
 require 'zlib'
 require 'json'
@@ -13,7 +13,7 @@ class HighLevelBrowse::DB
   # database with an efficient structure for querying
   # @param [Array<HighLevelBrowse::CallNumberRange>] array_of_ranges
   def initialize(array_of_ranges)
-    @all    = array_of_ranges
+    @all = array_of_ranges
     @ranges = self.create_letter_indexed_ranges(@all)
   end
 
@@ -22,8 +22,8 @@ class HighLevelBrowse::DB
   # @private
   def create_letter_indexed_ranges(all)
     bins = {}
-    ('A'..'Z').each do |letter|
-      cnrs         = all.find_all {|x| x.firstletter == letter}
+    ('a'..'z').each do |letter|
+      cnrs = all.find_all { |x| x.firstletter == letter }
       bins[letter] = HighLevelBrowse::CallNumberRangeSet.new(cnrs)
     end
     bins
@@ -40,15 +40,16 @@ class HighLevelBrowse::DB
   # @return [Array<Array>] A (possibly empty) array of arrays of topics
   def topics(*raw_callnumber_strings)
     raw_callnumber_strings.reduce([]) do |acc, raw_callnumber_string|
-      firstletter = raw_callnumber_string.strip.upcase[0]
+      firstletter = if raw_callnumber_string.nil?
+                      nil
+                    else raw_callnumber_string.to_s.strip.downcase[0]
+                    end
       if @ranges.has_key? firstletter
         acc + @ranges[firstletter].topics_for(raw_callnumber_string)
-      else
-        acc
+      else acc
       end
     end.uniq
   end
-
 
   alias_method :[], :topics
 
@@ -58,11 +59,10 @@ class HighLevelBrowse::DB
   #    (e.g., from 'https://www.lib.umich.edu/browse/categories/xml.php')
   # @return [DB]
   def self.new_from_xml(xml)
-    oga_doc_root         = Oga.parse_xml(xml)
-    simple_array_of_cnrs = cnrs_within_oga_node(node: oga_doc_root)
+    noko_doc_root = Nokogiri::XML(xml)
+    simple_array_of_cnrs = cnrs_within_noko_node(node: noko_doc_root)
     self.new(simple_array_of_cnrs).freeze
   end
-
 
   # Save to disk
   # @param [String] dir The directory where the hlb.json.gz file will be saved
@@ -73,7 +73,6 @@ class HighLevelBrowse::DB
     end
   end
 
-
   # Load from disk
   # @param [String] dir The directory where the hlb.json.gz file is located
   # @return [DB] The loaded database
@@ -81,11 +80,10 @@ class HighLevelBrowse::DB
     simple_array_of_cnrs = Zlib::GzipReader.open(File.join(dir, FILENAME)) do |infile|
       JSON.load(infile.read).to_a
     end
-    db                   = self.new(simple_array_of_cnrs)
+    db = self.new(simple_array_of_cnrs)
     db.freeze
     db
   end
-
 
   # Freeze everything
   # @return [DB] the frozen db
@@ -102,49 +100,44 @@ class HighLevelBrowse::DB
   #  * what the current topics are ([level1, level2])
   # Get all the call numbers assocaited with the topic represented by the given node,
   # as well as all the children of the given node, and send it back as a big ol' array
-  # @param [Oga::Node] node A node of the parsed HLB XML file
+  # @param [Nokogiri::XML::Node] node A node of the parsed HLB XML file
   # @param [Array<String>] decendent_xpaths A list of xpaths to the decendents of this node
   # @param [Array<String>] topic_array An array with all levels of the topics associated with this node
   # @return [Array<HighLevelBrowse::CallNumberRange>]
-  def self.cnrs_within_oga_node(node:, decendent_xpaths: ['/hlb/subject', 'topic', 'sub-topic'], topic_array: [])
+  def self.cnrs_within_noko_node(node:, decendent_xpaths: ['/hlb/subject', 'topic', 'sub-topic'], topic_array: [])
     if decendent_xpaths.empty?
       [] # base case -- we're as low as we're going to go
-    else
-      current_xpath_component = decendent_xpaths[0]
-      new_xpath               = decendent_xpaths[1..-1]
-      new_topic               = topic_array.dup
-      new_topic.push node.get(:name) unless node == node.root_node # skip the root
-      cnrs = []
-      # For each sub-component, get both the call-number-ranges (cnrs) assocaited
-      # with this level, as well as recusively getting from all the children
-      node.xpath(current_xpath_component).each do |c|
-        cnrs += call_numbers_list_from_leaves(node: c, topic_array: new_topic)
-        cnrs += cnrs_within_oga_node(node: c, decendent_xpaths: new_xpath, topic_array: new_topic)
-      end
-      cnrs
+    else current_xpath_component = decendent_xpaths[0]
+    new_xpath = decendent_xpaths[1..-1]
+    new_topic = topic_array.dup
+    new_topic.push node[:name] unless node == node.document # skip the root
+    cnrs = []
+    # For each sub-component, get both the call-number-ranges (cnrs) assocaited
+    # with this level, as well as recusively getting from all the children
+    node.xpath(current_xpath_component).each do |c|
+      cnrs += call_numbers_list_from_leaves(node: c, topic_array: new_topic)
+      cnrs += cnrs_within_noko_node(node: c, decendent_xpaths: new_xpath, topic_array: new_topic)
+    end
+    cnrs
     end
   end
-
 
   # Given a second-to-lowest-level node, get its topic and
   # extract call number ranges from its children
   def self.call_numbers_list_from_leaves(node:, topic_array:)
-    cnrs      = []
-    new_topic = topic_array.dup.push node.get(:name)
+    cnrs = []
+    new_topic = topic_array.dup.push node[:name]
     node.xpath('call-numbers').each do |cn_node|
-      min = cn_node.get(:start)
-      max = cn_node.get(:end)
+      min = cn_node[:start]
+      max = cn_node[:end]
 
       new_cnr = HighLevelBrowse::CallNumberRange.new(min: min, max: max, topic_array: new_topic)
       if new_cnr.illegal?
-        # do some sort of logging
-      else
-        cnrs.push new_cnr
+        # do some sort of logging else cnrs.push new_cnr
       end
     end
     cnrs
 
   end
-
 
 end
